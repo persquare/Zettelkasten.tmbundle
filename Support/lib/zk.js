1155eef83a81e338ba4d0dc1ce4e3c27cf28de55
Per Persson <>
2014-09-23 20:54:24 +0200

    Don't forget 'var'...

--- Support/lib/zk.js
+++ Support/lib/zk.js
@@ -1,14 +1,14 @@
 function display_note(id) {
-    preview = document.getElementById('preview_pane')
-    path = notes[id][0]['_path']
-    obj = TextMate.system('"Markdown.pl" "' + path +'"', null);
+    var preview = document.getElementById('preview_pane')
+    var path = notes[id][0]['_path']
+    var obj = TextMate.system('"Markdown.pl" "' + path +'"', null);
     preview.innerHTML = obj.outputString
 }
 
 function filter_results(search) {
-	str = search.value
-	results = document.getElementById('results')
-	count = 0
+	var str = search.value
+	var results = document.getElementById('results')
+	var count = 0
     // Simple filtering on ID example 
     for (var key in notes) {
         if (notes[key][0].ID.substring(0, str.length) === str) {
