89a236c79a8ecb792ed45d5ca533f0f1eb128e95
Per Persson <per.persson@ericsson.com>
2014-09-24 12:49:06 +0200

    Make zk://<id> URLs open in the browser display.

--- Support/lib/zk.js
+++ Support/lib/zk.js
@@ -4,12 +4,30 @@ function format_header(element, path) {
     header.innerHTML = '<div class="header"><pre>'+text+'</pre><a href="txmt://open?url=file://' + path + '">Edit…</a></div>';
 }
 
+function open_zk_link(element) {
+    var id = this.href.substring(5)
+    display_note(id);
+    return false;
+}
+
+function mangle_links(element) {
+  var links = element.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    var url = links[i].href;
+    var url_scheme = url.substring(0, 5);
+    if (url_scheme === 'zk://') {
+      links[i].onclick = open_zk_link;  
+    }
+  } 
+}
+
 function display_note(id) {
     var preview = document.getElementById('preview_pane')
     var path = notes[id][0]['_path']
     var obj = TextMate.system('"Markdown.pl" "' + path +'"', null);
     preview.innerHTML = obj.outputString
     format_header(preview, path);
+    mangle_links(preview)
 }
 
 function reset_list() {
